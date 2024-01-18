85f43b6c6c257297102df9a31b03fd4935fd5a6c
Mishra vibhakar <mishra.vibhakar7262@gmail.com>
2024-01-19 01:32:15 +0530

    all done

---
+++ script.js
@@ -0,0 +1,29 @@
+const button = document.querySelector("#btn");
+
+button.addEventListener('click', clickModular);
+
+let p = document.querySelector("#jokes");
+
+function clickModular(){
+    let joke = " ";
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', "https://api.api-ninjas.com/v1/dadjokes?limit=1");
+    xhr.setRequestHeader('x-Api-key', "/kKbBkDCP6NzStP5TmuARQ==weULnBZuaArhNg8n");
+    xhr.send();
+
+    button.innerText = "Fetching jokes...";
+
+    xhr.onload = function(){
+        if(xhr.status === 200){
+            const result = JSON.parse(xhr.responseText);
+            joke = result[0].joke;
+            
+            // update the jokes inside dome
+
+            p.innerText = joke;
+
+            button.innerText = "Tell Me A Joke";
+        }
+    };
+}
