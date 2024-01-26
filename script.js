8e88bf2ea26cc7365c3dcc6db34b8640517126f2
Mishra vibhakar <mishra.vibhakar7262@gmail.com>
2024-01-26 22:34:41 +0530

    "done"

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function clickModular(){
     xhr.send();
 
     button.innerText = "Fetching jokes...";
+    p.innerText = "loading....!!!";
 
     xhr.onload = function(){
         if(xhr.status === 200){
